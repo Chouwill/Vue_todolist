53196a10f061b3b70a883f0ed892b76a12eeb4fd
Chouwill <will92068@gmail.com>
2024-08-06 23:42:49 +0800

    Initial commit with build files

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Dream_Plan_CDN/',
   plugins: [vue()],
   resolve: {
     alias: {
@@ -13,10 +14,20 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:2500/api/",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
+        // 代理配置
       },
     },
   },
-})
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        },
+      },
+    },
+    chunkSizeWarningLimit: 1000, // 將塊大小警告限制調整為 1000 kB
+  },
+});
