54628a7af3f9f759bdf82b1e818c9bd10a322bbf
Chouwill <will92068@gmail.com>
2024-08-24 11:58:05 +0800

    08.24中午更新

--- src/page/Enjoylife.vue
+++ src/page/Enjoylife.vue
@@ -27,36 +27,40 @@ onMounted(async () => {
         <h2>國內旅遊</h2>
         <ul v-for="item in enjoyData" :key="item.id">
           <li>
-            <h4>澎湖花火節</h4>
-            <img :src="item.imageUrl" alt="">
+            <h4>大英博物館</h4>
+            <img :src="item.imageUrl" alt="" />
             <a href="#">查看活動</a>
           </li>
           <li>
-            <h4>澎湖花火節</h4>
-            <img :src="item.imagesUrl" alt="">
+            <h4>威尼斯水上嘉年華</h4>
+            <img :src="item.imagesUrl" alt="" />
             <a href="#">查看活動</a>
           </li>
           <li>
-            <h4>澎湖花火節</h4>
-            <img :src="item.imagesUrl[1]" alt="">
+            <h4>澎湖潛水漫步</h4>
+            <img :src="item.imagesUrl[1]" alt="" />
             <a href="#">查看活動</a>
           </li>
         </ul>
       </div>
       <div class="div">
-        <h2>國外美食</h2>
+        <h2>美食饗宴</h2>
         <ul v-for="item in enjoyData" :key="item.id">
           <li>
-            <h4>澎湖花火節</h4>
-            <img :src="item.imagesUrl[2]" alt="">
+            <h4>日式生魚片之旅</h4>
+            <img :src="item.imagesUrl[2]" alt="" />
+            <a href="#">查看活動</a>
           </li>
           <li>
-            <h4>澎湖花火節</h4>
-            <img :src="item.imagesUrl[3]" alt="">
+            <h4>法式牛排晚餐</h4>
+
+            <img :src="item.imagesUrl[3]" alt="" />
+            <a href="#">查看活動</a>
           </li>
           <li>
-            <h4>澎湖花火節</h4>
-            <img :src="item.imagesUrl[4]" alt="">
+            <h4>經典美式蘋果派</h4>
+            <img :src="item.imagesUrl[4]" alt="" />
+            <a href="#">查看活動</a>
           </li>
         </ul>
       </div>
@@ -65,8 +69,8 @@ onMounted(async () => {
 </template>
 
 <style lang="scss" scoped>
-*{
-    box-sizing: border-box;
+* {
+  box-sizing: border-box;
 }
 .container {
   width: 100%;
@@ -77,10 +81,10 @@ onMounted(async () => {
       display: flex;
       justify-content: center;
       align-items: center;
-    //   border: 5px solid olive;
+      border: 5px solid olive;
       flex-direction: column;
       h2 {
-        width: 48%;
+        width: 70%;
         text-align: left;
         border: 2px solid red;
       }
@@ -92,29 +96,67 @@ onMounted(async () => {
         border: 5px solid orange;
         justify-content: center;
         align-items: center;
-        li{
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            flex-direction: column;
-            border: 5px solid red;
-            position: relative;
-            h4{
-                text-align: center;
+        @media (max-width: 768px) {
+          width: 100%;
+          flex-wrap: wrap;
+          // height: 500px;
+        }
+        @media (max-width: 414px) {
+          width: 100%;
+          flex-wrap: wrap;
+          // height: 500px;
+        }
+        li {
+          width: 100%;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          flex-direction: column;
+          border: 5px solid red;
+          position: relative;
+          @media (max-width: 768px) {
+            width: 80%;
+            flex-wrap: wrap;
+            border: 5px solid rgb(25, 0, 255);
 
+            // height: 500px;
+          }
+          @media (max-width: 414px) {
+            width: 75%;
+            flex-wrap: wrap;
+            // height: 500px;
+            border: 5px solid orange;
+
+          }
+          h4 {
+            text-align: center;
+          }
+          img {
+            // max-width: 50%;
+            width: 200px;
+            height: 300px;
+            // height: 50%;
+          }
+          a {
+            width: 50%;
+            position: absolute;
+            background-color: #afbf30;
+            bottom: 0;
+            text-align: center;
+            font-weight: 700;
+            font-size: 15px;
+            padding: 10px 0;
+            @media (max-width: 768px) {
+              width: 61%;
+              flex-wrap: wrap;
+              // height: 500px;
             }
-            img{
-                max-width: 50%;
-                height: 50%;
+            @media (max-width: 414px) {
+              width: 78%;
+              flex-wrap: wrap;
+              // height: 500px;
             }
-            a{
-                width: 50%; 
-                position: absolute;
-                background-color: #AFBF30;
-                bottom: 0;
-                text-align: center;
-                padding: 10px 0;
-            }
+          }
         }
       }
     }
