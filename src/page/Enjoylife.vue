1abff8dca22721a19e2b458b0dfe7a00374861a4
Chouwill <will92068@gmail.com>
2024-09-17 23:38:29 +0800

    商城順序調換、增加活動連結

--- src/page/Enjoylife.vue
+++ src/page/Enjoylife.vue
@@ -32,17 +32,17 @@ onMounted(async () => {
           <li>
             <h4>大英博物館</h4>
             <img :src="item.imageUrl" alt="" />
-            <a href="#">查看活動</a>
+            <a href="https://britishmuseum.org.cn/" target="_blank">查看活動</a>
           </li>
           <li>
             <h4>威尼斯水上嘉年華</h4>
             <img :src="item.imagesUrl" alt="" />
-            <a href="#">查看活動</a>
+            <a href="https://event.liontravel.com/zh-tw/europe/carnival-of-venice" target="_blank">查看活動</a>
           </li>
           <li>
             <h4>澎湖潛水漫步</h4>
             <img :src="item.imagesUrl[1]" alt="" />
-            <a href="#">查看活動</a>
+            <a href="http://www.ezpenghu.com.tw/travel_10.htm" target="_blank">查看活動</a>
           </li>
         </ul>
       </div>
@@ -52,18 +52,18 @@ onMounted(async () => {
           <li>
             <h4>日式生魚片之旅</h4>
             <img :src="item.imagesUrl[2]" alt="" />
-            <a href="#">查看活動</a>
+            <a href="https://tabelog.com/tw/tokyo/A1302/A130202/13117221/" target="_blank">查看活動</a>
           </li>
           <li>
             <h4>法式牛排晚餐</h4>
 
             <img :src="item.imagesUrl[3]" alt="" />
-            <a href="#">查看活動</a>
+            <a href="https://www.cairns-stonegrill.com.tw/" target="_blank">查看活動</a>
           </li>
           <li>
             <h4>經典美式蘋果派</h4>
             <img :src="item.imagesUrl[4]" alt="" />
-            <a href="#">查看活動</a>
+            <a href="https://onepercent.storm.mg/article/4085378" target="_blank">查看活動</a>
           </li>
         </ul>
       </div>
--- src/page/Shopping.vue
+++ src/page/Shopping.vue
@@ -3,38 +3,11 @@
     <!-- <h2>閱讀助力，PRO加值。升級你的計畫，精選書籍與PRO會員帶你走得更遠</h2> -->
     <div class="page_title">
       <h2>加值服務商城</h2>
-      <h3>優質書籍推薦:</h3>
+      <!-- <h3>優質書籍推薦:</h3> -->
     </div>
-    <ul class="Books_product_list">
-      <li v-for="item in booksdata" :key="item.id">
-        <img :src="item.imageUrl" :alt="item.title" />
-        <div class="product_information">
-          <div class="product_text">
-            <h4>{{ item.title }}</h4>
-            <span>${{ item.price }}元</span>
-          </div>
-          <button @click="addToCart(item)">加入購物車</button>
-        </div>
-      </li>
-    </ul>
     <div class="page_title">
-      <h3>VIP尊榮方案服務:</h3>
-    </div>
-    <ul class="Vip_product_list">
-      <li v-for="item in VipData" :key="item.id">
-        <img :src="item.imageUrl" :alt="item.title" />
-        <div class="Vip_information">
-          <div class="Vip_text">
-            <h4>{{ item.title }}</h4>
-            <span>${{ item.price }}元</span>
-          </div>
-          <button @click="addToCart(item)">加入購物車</button>
-        </div>
-      </li>
-    </ul>
-    <div class="page_title">
+      <!-- <h3>優質書籍推薦:</h3> -->
       <h3>VIP尊榮方案簡介:</h3>
-      <!-- <p></p> -->
     </div>
     <ul class="Vip_Serve_list">
       <li v-for="item in VipServe" :key="item.id">
@@ -79,8 +52,48 @@
         </tbody>
       </table>
     </div>
+    <div class="page_title">
+      <!-- <h3>優質書籍推薦:</h3> -->
+      <h3>VIP尊榮方案服務:</h3>
+    </div>
+    <ul class="Vip_product_list">
+      <li v-for="item in VipData" :key="item.id">
+        <img :src="item.imageUrl" :alt="item.title" />
+        <div class="Vip_information">
+          <div class="Vip_text">
+            <h4>{{ item.title }}</h4>
+            <span>${{ item.price }}元</span>
+          </div>
+          <button @click="addToCart(item)">加入購物車</button>
+        </div>
+      </li>
+    </ul>
+    <div class="page_title">
+      <h3>優質書籍推薦:</h3>
+    </div>
+    <ul class="Books_product_list">
+      <li v-for="item in booksdata" :key="item.id">
+        <img :src="item.imageUrl" :alt="item.title" />
+        <div class="product_information">
+          <div class="product_text">
+            <h4>{{ item.title }}</h4>
+            <span>${{ item.price }}元</span>
+          </div>
+          <button @click="addToCart(item)">加入購物車</button>
+        </div>
+      </li>
+    </ul>
+    <!-- <div class="page_title">
+      <h3>VIP尊榮方案服務:</h3>
+    </div>
+
+    <div class="page_title">
+      <h3>VIP尊榮方案簡介:</h3>
+      <p></p>
+    </div> -->
+
     <aside>
-      <div class="shopping_cart"></div>
+      <!-- <div class="shopping_cart">999</div> -->
     </aside>
     <el-dialog
       v-model="productInfo.visible"
@@ -211,6 +224,255 @@ onMounted(async () => {
       }
     }
   }
+  .Vip_Serve_list {
+    width: 1141px;
+    max-width: 60%;
+    display: flex;
+    justify-content: center;
+    gap: 15px;
+    flex-wrap: wrap;
+    padding: 0 24px;
+    margin-bottom: 3rem;
+    // border: 3px solid palegreen;
+    @media (max-width: 768px) {
+      // background-color: rgb(55, 0, 255);
+      display: flex;
+      max-width: 90%;
+      flex-wrap: wrap;
+    }
+    @media (max-width: 414px) {
+      // background-color: rgb(0, 255, 47);
+      display: flex;
+      max-width: 100%;
+      flex-wrap: wrap;
+      gap: 30px;
+    }
+
+    li {
+      width: calc(100% - 30px) / 3;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      flex-direction: column;
+      position: relative;
+      box-shadow: 0 4px 12px rgba(0, 0, 0, 0.2); /* 加深陰影效果 */
+      @media (max-width: 768px) {
+        // background-color: rgb(0, 255, 89);
+        display: flex;
+        width: 40%;
+        flex-direction: column;
+      }
+      @media (max-width: 414px) {
+        // background-color: rgb(255, 0, 179);
+        display: flex;
+        width: 80%;
+        flex-direction: column;
+      }
+      img {
+        width: 200px;
+        height: 270px;
+        margin: 10px 0;
+        object-fit: cover;
+      }
+      .product_information {
+        padding: 10px 0;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        gap: 20px;
+        width: 300px;
+        .product_text {
+          width: 200px;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          gap: 10px;
+          // background-color: #4ba0d7;
+          padding: 5px 10px;
+          height: 60px;
+          position: absolute;
+          bottom: 0px;
+          opacity: 0.8;
+
+          h4 {
+            font-weight: 900;
+            font-size: 13px;
+            color: #000;
+            text-align: center;
+          }
+        }
+      }
+    }
+  }
+
+  .table-container {
+    max-width: 900px;
+    width: 50%;
+    margin: 50px auto;
+    padding: 20px;
+    background-color: #ffffff;
+    border-radius: 10px;
+    box-shadow: 0 6px 12px rgba(0, 0, 0, 0.1);
+    overflow: hidden;
+    @media (max-width: 768px) {
+      // border: 5px solid red;
+      max-width: 900px;
+      width: auto;
+    }
+  }
+  .Vip_product_list {
+    width: 1141px;
+    max-width: 60%;
+    display: flex;
+    justify-content: center;
+    gap: 15px;
+    flex-wrap: wrap;
+    padding: 0 24px;
+    margin-bottom: 3rem;
+    // border: 5px solid red;
+    @media (max-width: 768px) {
+      // background-color: rgb(55, 0, 255);
+      display: flex;
+      max-width: 90%;
+      flex-wrap: wrap;
+    }
+    @media (max-width: 414px) {
+      // background-color: rgb(0, 255, 47);
+      display: flex;
+      max-width: 100%;
+      flex-wrap: wrap;
+      gap: 30px;
+    }
+    li {
+      // width: calc(100% - 30px) / 3;
+      width: 30%;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      flex-direction: column;
+      // border: 5px solid yellow;
+      position: relative;
+      box-shadow: 0 4px 12px rgba(0, 0, 0, 0.2); /* 加深陰影效果 */
+      @media (max-width: 768px) {
+        // background-color: rgb(0, 255, 89);
+        display: flex;
+        width: 40%;
+        flex-direction: column;
+      }
+      @media (max-width: 414px) {
+        // background-color: rgb(255, 85, 0);
+        display: flex;
+        width: 80%;
+        flex-direction: column;
+      }
+      img {
+        width: 200px;
+        height: 270px;
+        margin: 10px 0;
+        object-fit: cover;
+      }
+      .Vip_information {
+        padding: 10px 0;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        gap: 20px;
+        width: 300px;
+        // border: 5px solid green;
+        .Vip_text {
+          width: 200px;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          gap: 10px;
+          // background-color: #4ba0d7;
+          padding: 5px 10px;
+          height: 60px;
+          position: absolute;
+          margin: 55px 0 0 0;
+          bottom: 55px;
+          opacity: 0.8;
+          // border: 5px solid green;
+
+          h4,
+          span {
+            font-weight: 900;
+            font-size: 13px; //太大每個購物車按鈕會不一樣
+            color: #000;
+            text-align: center;
+          }
+        }
+        button {
+          width: 100px;
+          // height: 41px;
+          padding: 10px 10px;
+          border-radius: 5px;
+          border: none;
+          font-weight: 700;
+          font-size: 16px;
+          color: #ffffff;
+          background-color: #4ba0d7;
+          transition: background-color 0.3s ease;
+          cursor: pointer;
+
+          &:hover {
+            background-color: #3a8bb5;
+          }
+          &:active {
+            background-color: #3a8bb5;
+            box-shadow: inset 4px 10px 8px rgba(0, 0, 0, 0.2); /* 按壓效果 */
+          }
+        }
+      }
+    }
+  }
+  .membership-table {
+    width: 100%;
+    border-collapse: collapse;
+    font-family: "Arial", sans-serif;
+    text-align: left;
+    font-size: 16px;
+    line-height: 1.6;
+
+    thead {
+      // background-color: #f5e4d0;
+      background-color: #82bae0;
+      font-weight: 700;
+      color: #0c0702; // 深灰色文字
+      text-align: center;
+
+      th {
+        padding: 15px;
+        text-transform: uppercase;
+        letter-spacing: 0.1em;
+      }
+    }
+
+    tbody {
+      tr {
+        &:nth-child(even) {
+          background-color: #f9f9f9;
+        }
+
+        &:hover {
+          background-color: #f1f1f1;
+        }
+
+        td {
+          text-align: center;
+          padding: 15px;
+          border-bottom: 1px solid #e0e0e0;
+          transition: background-color 0.3s;
+
+          &:first-child {
+            font-weight: bold;
+            color: #333;
+            text-align: center;
+          }
+        }
+      }
+    }
+  }
 
   .Books_product_list {
     max-width: 60%;
@@ -329,256 +591,6 @@ onMounted(async () => {
           &:active {
             background-color: #3a8bb5;
             box-shadow: inset 4px 10px 8px rgba(0, 0, 0, 0.2); /* 按壓效果 */
-          }
-        }
-      }
-    }
-  }
-  .Vip_product_list {
-    width: 1141px;
-    max-width: 60%;
-    display: flex;
-    justify-content: center;
-    gap: 15px;
-    flex-wrap: wrap;
-    padding: 0 24px;
-    margin-bottom: 3rem;
-    // border: 5px solid red;
-    @media (max-width: 768px) {
-      // background-color: rgb(55, 0, 255);
-      display: flex;
-      max-width: 90%;
-      flex-wrap: wrap;
-    }
-    @media (max-width: 414px) {
-      // background-color: rgb(0, 255, 47);
-      display: flex;
-      max-width: 100%;
-      flex-wrap: wrap;
-      gap: 30px;
-    }
-    li {
-      // width: calc(100% - 30px) / 3;
-      width: 30%;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      flex-direction: column;
-      // border: 5px solid yellow;
-      position: relative;
-      box-shadow: 0 4px 12px rgba(0, 0, 0, 0.2); /* 加深陰影效果 */
-      @media (max-width: 768px) {
-        // background-color: rgb(0, 255, 89);
-        display: flex;
-        width: 40%;
-        flex-direction: column;
-      }
-      @media (max-width: 414px) {
-        // background-color: rgb(255, 85, 0);
-        display: flex;
-        width: 80%;
-        flex-direction: column;
-      }
-      img {
-        width: 200px;
-        height: 270px;
-        margin: 10px 0;
-        object-fit: cover;
-      }
-      .Vip_information {
-        padding: 10px 0;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        gap: 20px;
-        width: 300px;
-        // border: 5px solid green;
-        .Vip_text {
-          width: 200px;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          gap: 10px;
-          // background-color: #4ba0d7;
-          padding: 5px 10px;
-          height: 60px;
-          position: absolute;
-          margin: 55px 0 0 0;
-          bottom: 55px;
-          opacity: 0.8;
-          // border: 5px solid green;
-
-          h4,
-          span {
-            font-weight: 900;
-            font-size: 13px; //太大每個購物車按鈕會不一樣
-            color: #000;
-            text-align: center;
-          }
-        }
-        button {
-          width: 100px;
-          // height: 41px;
-          padding: 10px 10px;
-          border-radius: 5px;
-          border: none;
-          font-weight: 700;
-          font-size: 16px;
-          color: #ffffff;
-          background-color: #4ba0d7;
-          transition: background-color 0.3s ease;
-          cursor: pointer;
-
-          &:hover {
-            background-color: #3a8bb5;
-          }
-          &:active {
-            background-color: #3a8bb5;
-            box-shadow: inset 4px 10px 8px rgba(0, 0, 0, 0.2); /* 按壓效果 */
-          }
-        }
-      }
-    }
-  }
-  .Vip_Serve_list {
-    width: 1141px;
-    max-width: 60%;
-    display: flex;
-    justify-content: center;
-    gap: 15px;
-    flex-wrap: wrap;
-    padding: 0 24px;
-    margin-bottom: 3rem;
-    // border: 3px solid palegreen;
-    @media (max-width: 768px) {
-      // background-color: rgb(55, 0, 255);
-      display: flex;
-      max-width: 90%;
-      flex-wrap: wrap;
-    }
-    @media (max-width: 414px) {
-      // background-color: rgb(0, 255, 47);
-      display: flex;
-      max-width: 100%;
-      flex-wrap: wrap;
-      gap: 30px;
-    }
-
-    li {
-      width: calc(100% - 30px) / 3;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      flex-direction: column;
-      position: relative;
-      box-shadow: 0 4px 12px rgba(0, 0, 0, 0.2); /* 加深陰影效果 */
-      @media (max-width: 768px) {
-        // background-color: rgb(0, 255, 89);
-        display: flex;
-        width: 40%;
-        flex-direction: column;
-      }
-      @media (max-width: 414px) {
-        // background-color: rgb(255, 0, 179);
-        display: flex;
-        width: 80%;
-        flex-direction: column;
-      }
-      img {
-        width: 200px;
-        height: 270px;
-        margin: 10px 0;
-        object-fit: cover;
-      }
-      .product_information {
-        padding: 10px 0;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        gap: 20px;
-        width: 300px;
-        .product_text {
-          width: 200px;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          gap: 10px;
-          // background-color: #4ba0d7;
-          padding: 5px 10px;
-          height: 60px;
-          position: absolute;
-          bottom: 0px;
-          opacity: 0.8;
-
-          h4 {
-            font-weight: 900;
-            font-size: 13px;
-            color: #000;
-            text-align: center;
-          }
-        }
-      }
-    }
-  }
-
-  .table-container {
-    max-width: 900px;
-    width: 50%;
-    margin: 50px auto;
-    padding: 20px;
-    background-color: #ffffff;
-    border-radius: 10px;
-    box-shadow: 0 6px 12px rgba(0, 0, 0, 0.1);
-    overflow: hidden;
-    @media (max-width: 768px) {
-      // border: 5px solid red;
-      max-width: 900px;
-      width: auto;
-    }
-  }
-
-  .membership-table {
-    width: 100%;
-    border-collapse: collapse;
-    font-family: "Arial", sans-serif;
-    text-align: left;
-    font-size: 16px;
-    line-height: 1.6;
-
-    thead {
-      // background-color: #f5e4d0;
-      background-color: #82bae0;
-      font-weight: 700;
-      color: #0c0702; // 深灰色文字
-      text-align: center;
-
-      th {
-        padding: 15px;
-        text-transform: uppercase;
-        letter-spacing: 0.1em;
-      }
-    }
-
-    tbody {
-      tr {
-        &:nth-child(even) {
-          background-color: #f9f9f9;
-        }
-
-        &:hover {
-          background-color: #f1f1f1;
-        }
-
-        td {
-          text-align: center;
-          padding: 15px;
-          border-bottom: 1px solid #e0e0e0;
-          transition: background-color 0.3s;
-
-          &:first-child {
-            font-weight: bold;
-            color: #333;
-            text-align: center;
           }
         }
       }
